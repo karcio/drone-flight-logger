cc9f8ef1625f1e011bfe1e9b3a52ba230d256df4
karcio <karcio@pool-ipv6-pd.home>
2020-04-24 20:10:16 +0100

    update .travis.yml

--- script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "... insert sample user ..."
-psql -U postgres -d rcdatadb -c "INSERT INTO users (username,password) VALUES ('user1','password')"
-psql -U postgres -d rcdatadb -c "SELECT * FROM users"
