417f895f10051fef16d83cf7e250e23f79ff0eed
karcio <karcio@pool-ipv6-pd.home>
2020-04-24 20:03:26 +0100

    update .travis.yml

---
+++ db/db.sql
@@ -0,0 +1,7 @@
+CREATE TABLE users(
+   id serial PRIMARY KEY,
+   username VARCHAR (50) UNIQUE NOT NULL,
+   password VARCHAR (50) NOT NULL
+);
+INSERT INTO users (username,password) VALUES ('user1','password');
+SELECT * FROM USERS;
